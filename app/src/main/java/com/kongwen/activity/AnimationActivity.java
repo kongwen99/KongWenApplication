796eab7b323f87573b4d15b0b94ae4f46c0ebd47
kongwen99 <kongwen19920814@163.com>
2018-06-01 14:26:47 +0800

    view动画和帧动画

--- app/src/main/java/com/kongwen/activity/AnimationActivity.java
+++ app/src/main/java/com/kongwen/activity/AnimationActivity.java
@@ -1,8 +1,10 @@
 package com.kongwen.activity;
 
+import android.animation.ObjectAnimator;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 
@@ -12,7 +14,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
- * android动画
+ * android动画(MainActivity中使用了LayoutAnimation)
  * 1、view动画：平移动画，所放动画，旋转动画，透明度动画；
  * 2、帧动画：按顺序播放一系列图片
  * 3、属性动画：动态改变对象的属性
@@ -23,6 +25,8 @@ public class AnimationActivity extends AppCompatActivity {
     Button btn1;
     @BindView(R.id.btn2)
     Button btn2;
+    @BindView(R.id.btn3)
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,16 @@ public class AnimationActivity extends AppCompatActivity {
 
         initViewAnimation();
         initFrameAnimation();
+        performAnimate();
+    }
+
+    /**
+     * 属性动画
+     */
+    private void performAnimate() {
+        ViewWapper wrapper = new ViewWapper(btn3);
+        ObjectAnimator.ofInt(wrapper, "width", 500).setDuration(1000).start();
+
     }
 
     /**
@@ -54,5 +68,22 @@ public class AnimationActivity extends AppCompatActivity {
         AlphaAnimation animation = new AlphaAnimation(0, 1);
         animation.setDuration(3000);
         btn1.startAnimation(animation);
+    }
+
+    class ViewWapper {
+        private View mTarget;
+
+        public ViewWapper(View mTarget) {
+            this.mTarget = mTarget;
+        }
+
+        public int getWidth() {
+            return mTarget.getLayoutParams().width;
+        }
+
+        public void setWidth(int width) {
+            mTarget.getLayoutParams().width = width;
+            mTarget.requestLayout();//重新布局view
+        }
     }
 }
